a8f029eda4d954039ec691f4e6b5bd833ed200d9
M-D-M <m.d.mcnamara@gmail.com>
2020-04-07 18:54:45 -0400

    Initial changes to support slack python module v2.0.

--- drakebot.py
+++ drakebot.py
@@ -1,20 +1,16 @@
-#!/usr/bin/env python3
+#!python3
 
-import os, time, re, random, eliza
-from slackclient import SlackClient
+import os
+import time
+import re
+import random
+import slack
+import eliza
+import ENV
 
-# instantiate Slack client
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-
-# instantiate eliza
-eliza_mode = 0
-therapist = eliza.eliza()
-
-# drakebot's user ID in Slack: value is assigned after the bot starts up
-drakebot_id = None
 
 # constants
-RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+BOT_NAME = 'Drake Bot'
 EXAMPLE_COMMAND = "give me a drake fact"
 MENTION_REGEX = r"^.*<@(|[WU].+?)>\W?(.*)"
 DRAKE_FACT_FILE = 'drakebot_facts.txt'
@@ -29,6 +25,7 @@ DRAKE_REGEXES = {
 }
 DEFAULT_RESPONSE = "Not sure what you mean. Try *{}*, or *tell <name> they tha best*".format(EXAMPLE_COMMAND)
 
+
 def random_drake_fact():
 	afile = open(DRAKE_FACT_FILE)
 	line = next(afile)
@@ -37,12 +34,23 @@ def random_drake_fact():
 		line = aline
 	return line
 
+
 def parse_bot_commands(slack_events):
 	"""
 		Parses a list of events coming from the Slack RTM API to find bot commands.
 		If a bot command is found, this function returns a tuple of command and channel.
 		If its not found, then this function returns None, None.
 	"""
+
+	def parse_direct_mention(message_text):
+		"""
+			Finds a direct mention (a mention that is at the beginning) in message text
+			and returns the user ID which was mentioned. If there is no direct mention, returns None
+		"""
+		matches = re.search(MENTION_REGEX, message_text)
+		# the first group contains the username, the second group contains the remaining message
+		return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
 	for event in slack_events:
 		if event["type"] == "message" and not "subtype" in event:
 			user_id, message = parse_direct_mention(event["text"])
@@ -50,16 +58,8 @@ def parse_bot_commands(slack_events):
 				return message, event["channel"]
 	return None, None
 
-def parse_direct_mention(message_text):
-	"""
-		Finds a direct mention (a mention that is at the beginning) in message text
-		and returns the user ID which was mentioned. If there is no direct mention, returns None
-	"""
-	matches = re.search(MENTION_REGEX, message_text)
-	# the first group contains the username, the second group contains the remaining message
-	return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
-def handle_command(command, channel):
+def old_handle_command(command, channel):
 	"""
 		Executes bot command if the command is known
 	"""
@@ -80,7 +80,7 @@ def handle_command(command, channel):
 		elif DRAKE_REGEXES["yolo"].match(command):
 			response = "That's the motto"
 		elif DRAKE_REGEXES["love"].match(command):
-			response = "Only partly.  I only love my bed and momma; I'm sorry."
+			response = "Only paresponse = Nonertly.  I only love my bed and momma; I'm sorry."
 		elif DRAKE_REGEXES["eliza_on"].match(command):
 			eliza_mode = 1
 			response = therapist.respond("Hello")
@@ -103,16 +103,80 @@ def handle_command(command, channel):
 		text=response or DEFAULT_RESPONSE
 	)
 
+
+@slack.RTMClient.run_on(event='message')
+def handle_command(**payload):
+
+	global eliza_mode
+	response = None
+	data = payload['data']
+
+	# data.get('subtype') in ['message_replied', 'bot_message']
+
+	if (data.get('subtype') not in ['message_replied', 'bot_message']):
+		channel_id = data['channel']
+		thread_ts = data['ts']
+		user = data['user']
+		webclient = payload['web_client']
+
+		command = data['text']
+
+		# This is where you start to implement more commands!
+		try:
+			if DRAKE_REGEXES["fact"].match(command) is not None:
+				response = random_drake_fact()
+			elif DRAKE_REGEXES["best"].match(command):
+				response = "{}, you da best, you da you da best".format((DRAKE_REGEXES["best"].match(command)).group(1))
+			elif DRAKE_REGEXES["hi"].match(command):
+				response = "What up"
+			elif DRAKE_REGEXES["yolo"].match(command):
+				response = "That's the motto"
+			elif DRAKE_REGEXES["love"].match(command):
+				response = "Only paresponse = Nonertly.  I only love my bed and momma; I'm sorry."
+			elif DRAKE_REGEXES["eliza_on"].match(command):
+				eliza_mode = 1
+				response = therapist.respond("Hello")
+			elif DRAKE_REGEXES["eliza_off"].match(command):
+				eliza_mode = 0
+				response = therapist.respond("quit")
+			else:
+				if eliza_mode is 1:
+					response = therapist.respond(command)
+				else:
+					response = DEFAULT_RESPONSE
+		except Exception as e:
+			response = "I have crashed! Help!"
+			print(str(e))
+
+		webclient.chat_postMessage(
+			channel=channel_id
+			,text = response or DEFAULT_RESPONSE
+			# ,thread_ts = thread_ts
+		)
+	elif (data.get('subtype') == 'message_replied'):
+		print('No "text" parameter detected in payload.')
+	elif (data.get('subtype') == 'bot_message'):
+		print('This is a response from the bot.  Ignore this.')
+
+
 if __name__ == "__main__":
-	if slack_client.rtm_connect(with_team_state=False):
-		print("Drake Bot connected and running!")
-		# Read bot's user ID by calling Web API method `auth.test`
-		drakebot_id = slack_client.api_call("auth.test")["user_id"]
-		while True:
-			command, channel = parse_bot_commands(slack_client.rtm_read())
-			if command:
-				print("DEBUG: " + command)
-				handle_command(command, channel)
-			time.sleep(RTM_READ_DELAY)
-	else:
-		print("Connection failed. Exception traceback printed above.")
+	slack_token = ENV.SLACK_BOT_TOKEN
+
+	if (slack_token is None):
+		raise Exception(f'''No value found for environment variable "SLACK_BOT_TOKEN"! 
+	Did you forget to activate the python environment, or create the env.bash file with necessary environment variables?
+	''')
+
+	rtmclient = slack.RTMClient(token=slack_token)
+
+	# instantiate eliza
+	eliza_mode = 0
+	therapist = eliza.eliza()
+
+	# drakebot's user ID in Slack: value is assigned after the bot starts up
+	drakebot_id = None
+
+	try:
+		rtmclient.start()
+	except Exception as e:
+		print(f"{e.__class__.__name__}: {e}\n\nConnection failed. Exception traceback printed above.")
