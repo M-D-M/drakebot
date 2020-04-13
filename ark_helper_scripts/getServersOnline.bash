76a70ac60499ff7ac2b580a4f08952b42bde56d0
M-D-M <m.d.mcnamara@gmail.com>
2020-04-13 19:26:39 -0400

    Modified getServersOnline.bash helper script to correct typo in sed command.

--- ark_helper_scripts/getServersOnline.bash
+++ ark_helper_scripts/getServersOnline.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-LEGEND_TEXT="[ Legend: X (days) - XX (hours) : XX (minutes) : XX (seconds) ]\n"
+LEGEND_TEXT="[ _Legend: X (days) - XX (hours) : XX (minutes) : XX (seconds)_ ]\n"
 
-Servers_Online=$(ps f | egrep ShooterGame | sed -n -e "s/^\([^ ]*\).*SessionName=\([^\?]*\)\?.*/\1=\2/p")
+Servers_Online=$(ps f | egrep ShooterGame | sed -n -e "s/^ *\([^ ]*\).*SessionName=\([^\?]*\)\?.*/\1=\2/p")
 amt_of_servers=$(echo $Servers_Online | tr -cd '=' | wc -c)
 
 if [[ $amt_of_servers -lt 1 ]]; then
     printf "No servers are currently online."
 else
-    printf "There are $amt_of_servers server(s) currently online.\n\n"
+    printf "There are *${amt_of_servers}* server(s) currently online.\n\n"
     printf "$LEGEND_TEXT"
     for Server in $Servers_Online
     do
@@ -16,6 +16,6 @@ else
         serv_name=$(printf $Server | cut -d"=" -f2)
         proc_lifetime=$(ps -o etime= -p $proc_id | xargs) # xargs trims whitespace
 
-        printf "\nServer Name: $serv_name (Uptime: $proc_lifetime)"
+        printf "\nServer Name: $serv_name (_Uptime: ${proc_lifetime}_)"
     done
 fi
