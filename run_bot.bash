ed64e9982daefdbab1dd4e5875d9e63b2c7f84c0
M-D-M <m.d.mcnamara@gmail.com>
2020-04-22 20:34:30 -0400

    Modified run_bot.bash to correctly look for existing process before launching new process.

--- run_bot.bash
+++ run_bot.bash
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 BASE_DIR=$(dirname $0)
+PYTHON_PROC_NAME='run_bot.py'
 
-source ${BASE_DIR}/env/bin/activate
+exit_code=1
+existing_process=$(ps ax | egrep -v "awk|grep" | grep "$PYTHON_PROC_NAME" | awk '{print $1}')
 
-python3 ${BASE_DIR}/run_bot.py &
+if [[ $existing_process == "" ]]; then
+    printf "Starting Bot!\n"
+    source ${BASE_DIR}/env/bin/activate
+    python3 ${BASE_DIR}/${PYTHON_PROC_NAME} &
+    exit_code=$?
+else
+    printf "Bot already running with Proc ID ${existing_process}!\n"
+    exit_code=11
+fi
+
+exit $exit_code
 
 # deactivate
